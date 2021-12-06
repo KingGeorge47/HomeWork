51470a5923b6c931dc2e9e0f81580f9a528bd263
TAMUNOSAKI GEORGE <77535380+KingGeorge47@users.noreply.github.com>
2021-12-05 17:30:04 -0800

    new update1

--- main.tf
+++ main.tf
@@ -2,5 +2,44 @@ resource "aws_s3_bucket" "Test_Team_Bucket" {
   bucket = var.test-bucket
   acl    = "private"
 
+  server_side_encryption_configuration {
+    rule {
+        apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.S3KMSkey.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+
   tags = {Name = "My bucket", Environment = "Dev"}
 }
+
+resource "aws_kms_key" "S3KMSkey" {
+  description         = "This key is used to encrypt bucket objects"
+  enable_key_rotation = true
+  policy = data.aws_iam_policy_document.key_policy.json
+}
+
+data "aws_caller_identity" "current"{}
+
+data "aws_iam_policy_document" "key_policy" {
+  statement {
+    sid = "grant root user full access"
+    effect = "Allow"
+    principals {
+      type = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+    actions = ["kms:*"]
+
+    resources = ["*"]
+    }
+
+    depends_on = [
+      aws_kms_key.S3KMSkey
+    ]
+
+}
+
+
+
